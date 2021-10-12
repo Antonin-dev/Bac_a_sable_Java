f76537adc17da76d09d0bc4823646f4f9d3d8a5d
antonin <antonin.val@gmail.com>
2021-10-12 11:07:31 +0200

    exo 5 done

--- exo5/Main.java
+++ exo5/Main.java
@@ -1,2 +1,12 @@
-package exo5;public class Main {
+package exo5;
+
+public class Main {
+    public static void main (String[] args){
+        int value = Integer.parseInt(args[0]);
+        int counter = 0;
+        while (counter != value){
+            System.out.println(counter);
+            counter++;
+        }
+    }
 }
